b8a3b5e7844392adbfeca01334744be788474004
Paulo Spiguel <41386612+PauloSpiguel@users.noreply.github.com>
2018-11-20 17:02:13 -0200

    Configuração de Asesso ao DB

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,5 +6,5 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    '' => array($vendorDir . '/hcodebr/php-classes/src'),
+    'NewTech\\' => array($vendorDir . '/hcodebr/php-classes/src'),
 );
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,8 +6,18 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit30493aef559194abbbaa44270c6653ac
 {
-    public static $fallbackDirsPsr4 = array (
-        0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+    public static $prefixLengthsPsr4 = array (
+        'N' => 
+        array (
+            'NewTech\\' => 8,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'NewTech\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+        ),
     );
 
     public static $prefixesPsr0 = array (
@@ -41,7 +51,8 @@ class ComposerStaticInit30493aef559194abbbaa44270c6653ac
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->fallbackDirsPsr4 = ComposerStaticInit30493aef559194abbbaa44270c6653ac::$fallbackDirsPsr4;
+            $loader->prefixLengthsPsr4 = ComposerStaticInit30493aef559194abbbaa44270c6653ac::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit30493aef559194abbbaa44270c6653ac::$prefixDirsPsr4;
             $loader->prefixesPsr0 = ComposerStaticInit30493aef559194abbbaa44270c6653ac::$prefixesPsr0;
             $loader->classMap = ComposerStaticInit30493aef559194abbbaa44270c6653ac::$classMap;
 
